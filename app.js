fc6455ba512ba7ff0157e2efc33459629ae425eb
sumitnair26 <sumit.nair26@gmail.com>
2019-08-28 09:11:46 +0530

    started learning node js

---
+++ app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body><h2>Hello !!!! from my Node.js Server ddsdsdsds</h2></body>');
+        res.write('<body><form action="/message" method="POST" ><input type="text"><button type="submit" name="message">Send</form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+   res.setHeader('Content-Type','text/html');
+   res.write('<html>');
+   res.write('<head><title>My First Node Page</title></head>');
+   res.write('<body><h2>Hello !!!! from my Node.js Serve</h2></body>');
+   res.write('</html>');
+   res.end();
+});
+
+server.listen(9500);
