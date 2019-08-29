50e7a829eaf2b6cc0b17a4df2677908f8b8a0fe8
sumitnair26 <sumit.nair26@gmail.com>
2019-08-29 13:12:52 +0530

    page not found

--- app.js
+++ app.js
@@ -14,4 +14,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(5555);
