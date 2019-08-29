afdfce5d88e34de41ca4b0e8308d193918b4445b
sumitnair26 <sumit.nair26@gmail.com>
2019-08-29 11:59:00 +0530

    expressJS body-parser middleware and request

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const http = require('http');
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((rq, res, next) => {
-    console.log("In the missleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((rq, res, next) => {
-    console.log("In another the missleware");
+app.post('/product',(req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/',(req, res, next) => {
     res.send('<h1>Hello from Express!!!</h1>');
 });
 
 const server = http.createServer(app);
 
-server.listen(8111);
+server.listen(5555);
