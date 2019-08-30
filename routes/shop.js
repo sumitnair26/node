3774af25baf4f5feacb8b1d91d3e9597d274b4ca
sumitnair26 <sumit.nair26@gmail.com>
2019-08-30 12:55:11 +0530

    added main-layout file

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get('/',(req, res, next) => {
     //console.log('shop js', adminData.products);
     //res.sendFile(path.join(rootDir,'views', 'shop.html'));
     const products = adminData.products;
-    res.render('shop', {prods: products, docTitle: 'Shop'}); //pug view
+    res.render('shop', {prods: products, docTitle: 'Shop', path:'/',pageTitle:'Shop'}); //pug view
 });
 
 module.exports = router;
