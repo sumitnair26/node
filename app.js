50fddfa95e7753f3b3aea9a601e741c7bf0d8c86
sumitnair26 <sumit.nair26@gmail.com>
2019-09-08 18:25:18 +0530

    the Layout with Partials

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.render('404',{pageTitle: '404 Page'});
+    res.render('404',{pageTitle: '404 Page', path:''});
 });
 
 app.listen(5550);
