1909a44c1d19b9522deb19788e27bff69c762c4b
sumitnair26 <sumit.nair26@gmail.com>
2019-09-16 16:58:43 +0530

    added controlers

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.render('404',{pageTitle: '404 Page', path:''});
-});
+app.use(errorController.get404);
 
-app.listen(5550);
+app.listen(8965);
--- routes/shop.js
+++ routes/shop.js
@@ -2,16 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-
 const router = express.Router();
-const adminData = require('./admin');
 
-router.get('/',(req, res, next) => {
-    //console.log('shop js', adminData.products);
-    //res.sendFile(path.join(rootDir,'views', 'shop.html'));
-    const products = adminData.products;
-    res.render('shop', {prods: products, docTitle: 'Shop', path:'/',pageTitle:'Shop'}); //pug view
-});
+const productsController = require('../controllers/products');
+
+router.get('/',productsController.getProducts);
 
 module.exports = router;
