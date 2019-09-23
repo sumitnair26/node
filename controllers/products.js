e672263c540e67f4fc71b9b00e9889f005535fab
sumitnair26 <sumit.nair26@gmail.com>
2019-09-23 19:19:54 +0530

    added pages

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product',{
+    res.render('admin/add-product',{
         pageTitle: 'Add Product',
         path:'/admin/add-product',
         formsCSS: true,
@@ -22,7 +22,7 @@ exports.getProducts = (req, res, next) => {
     //res.sendFile(path.join(rootDir,'views', 'shop.html'));
     //const products = adminData.products;
     Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             docTitle: 'Shop',
             path:'/',
