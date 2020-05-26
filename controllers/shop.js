8cafe995318088558d5d34d4b73dc1e75ad0530d
sumitnair26 <sumit.nair26@gmail.com>
2020-05-26 14:03:40 +0530

    show added cart items in cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,9 +38,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart =>{
+        Product.fetchAll(products=> {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if(cartProductData){
+                    cartProducts.push({productData: product, qty:cartProductData.qty });
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        });
     });
 };
 
@@ -50,6 +62,10 @@ exports.postCart = (req, res, next) => {
       Cart.addProduct(prodId, product.price);
     });
     res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
 };
 
 exports.getCheckout = (req, res, next) => {
