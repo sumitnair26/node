d9553b025e2e01ed7b5ecaa78dcfab382655936b
sumitnair26 <sumit.nair26@gmail.com>
2020-06-02 14:24:22 +0530

    connecting to sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'bro4u'
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-complete',
+//     password:'bro4u'
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete','root', 'bro4u', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+ module.exports = sequelize;
