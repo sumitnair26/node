feea86526de2982e25e4830a105762bbaf3101e2
sumitnair26 <sumit.nair26@gmail.com>
2020-06-25 18:25:08 +0530

    display orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -151,15 +151,26 @@ exports.postOrder = (req, res, next ) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user
+  .getOrders({include : ['products']})
+  .then(
+    orders=>{
+      res.render(
+        res.render('shop/orders', {
+          path: '/orders',
+          pageTitle: 'Your Orders',
+          orders: orders
+        })
+      )
+    }
+  ).catch(
+    err => console.log(err)
+  );
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+// exports.getCheckout = (req, res, next) => {
+//   res.render('shop/checkout', {
+//     path: '/checkout',
+//     pageTitle: 'Checkout'
+//   });
+// };
