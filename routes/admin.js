7d6b002241775062dc89a363b329db3e0408a5ec
sumitnair26 <sumit.nair26@gmail.com>
2020-07-28 14:27:28 +0530

    setting up update through mongo

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,7 @@ router.post('/add-product', adminController.postAddProduct);
 
 router.get('/products', adminController.getProducts);
 
-// router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 // router.post('/edit-product', adminController.postEditProduct);
 
